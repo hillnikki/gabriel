a235bc2ba10c9fa079a996a07f17a4eaf7f1c151
hillnikki <39469232+hillnikki@users.noreply.github.com>
2019-04-09 08:24:47 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,5 @@ function countdown() {
     var oneDay = 24*60*60*1000;
     var today = new Date();
     var wedding = new Date('May 18, 2019');
-    document.getElementById('countdownNumber').html = '<span>' + Math.round(Math.abs((today.getTime() - wedding.getTime())/(oneDay))) + '</span>';
+    document.getElementById('countdownNumber').html = Math.round(Math.abs((today.getTime() - wedding.getTime())/(oneDay)));
 }
